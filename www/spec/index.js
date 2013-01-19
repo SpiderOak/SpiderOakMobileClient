3409729c5df744f768080ed6f8825c9a9ccdfa28
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-01-19 16:55:14 +1100

    Added a test for nibbler to base32 encode usernames and share ids and organised some specs

--- www/spec/index.js
+++ www/spec/index.js
@@ -14,24 +14,42 @@ describe('spiderOakApp', function() {
       expect(window._).toBeDefined();
     });
 
-    it('should have backbone', function() {
-      expect(window.Backbone).toBeDefined();
+    describe('backbone', function() {
+      it('should have backbone', function() {
+        expect(window.Backbone).toBeDefined();
+      });
+      it('should have backbone.basicauth', function() {
+        expect(window.Backbone.BasicAuth).toBeDefined();
+      });
     });
 
-    it('should have backbone.basicauth', function() {
-      expect(window.Backbone.BasicAuth).toBeDefined();
+    describe('nibbler', function() {
+      it('should have nibbler', function() {
+        expect(window.Nibbler).toBeDefined();
+      });
+      it('should properly encode a username or share id',function() {
+        // "my_share_id" -> "NV4V643IMFZGKX3JMQ"
+        //   as described here:
+        //   https://spideroak.com/faq/questions/37/how_do_i_use_the_spideroak_web_api/
+        var share_id = "my_share_id";
+        var nibbler = new window.Nibbler({
+          dataBits: 8,
+          codeBits: 5,
+          keyString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',
+          pad: ''
+        });
+        var encoded_share_id = nibbler.encode(share_id);
+        expect(encoded_share_id).toBe("NV4V643IMFZGKX3JMQ");
+      });
     });
 
-    it('should have nibbler', function() {
-      expect(window.Nibbler).toBeDefined();
-    });
-
-    // Remove if no longer using jQTouch
-    it('should have jqtouch', function() {
-      expect(window.$.jQTouch).toBeDefined();
-    });
-    it('should have jqtouch menusheet', function() {
-      expect(window.$.fn.menusheet).toBeDefined();
+    describe('jQTouch', function() {
+      it('should have jqtouch', function() {
+        expect(window.$.jQTouch).toBeDefined();
+      });
+      it('should have jqtouch menusheet', function() {
+        expect(window.$.fn.menusheet).toBeDefined();
+      });
     });
   });
 

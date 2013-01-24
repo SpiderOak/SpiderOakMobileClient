7d7a2b511c3c185027f341e92ebd4c17124f78c2
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-01-24 14:41:39 +1100

    Try again - spies, mocks and stubs

--- www/spec/index.js
+++ www/spec/index.js
@@ -33,6 +33,15 @@ describe('spiderOakApp', function() {
       it('should have sinon', function() {
         expect(window.sinon).toBeDefined();
       });
+      it('should have sinon spies', function() {
+        expect(window.sinon.spy).toBeDefined();
+      });
+      it('should have sinon mocks', function() {
+        expect(window.sinon.mock).toBeDefined();
+      });
+      it('should have sinon stubs', function() {
+        expect(window.sinon.stub).toBeDefined();
+      });
     });
 
     describe('nibbler', function() {

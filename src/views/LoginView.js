d4d79f383332901c0a7e78b2f06b0e89a6684f1f
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-01-25 15:09:10 +1100

    Some more UI for the login screen

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -12,7 +12,9 @@
   spiderOakApp.LoginView = Backbone.View.extend({
     el: "#login",
     events: {
-      // ...
+      'focus input': 'input_focusHandler',
+      'blur input': 'input_blurHandler',
+      'submit form': 'form_submitHandler'
     },
     initialize: function() {
       _.bindAll(this);
@@ -27,6 +29,17 @@
     },
     pageAnimationEnd_handler: function(event, data) {
       // ...
+    },
+    input_focusHandler: function(event) {
+      this.$('.login-logo').addClass('rotated');
+    },
+    input_blurHandler: function(event) {
+      this.$('.login-logo').removeClass('rotated');
+    },
+    form_submitHandler: function(event) {
+      this.$('input').blur();
+      // @FIXME: Repace with actual login procedure
+      window.jQT.goTo('#main','slidedown');
     }
   });
   spiderOakApp.loginView = new spiderOakApp.LoginView().render();

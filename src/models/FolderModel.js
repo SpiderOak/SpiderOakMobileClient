18218d075c9781485f7fb731b440aedca2233114
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-01-25 15:09:41 +1100

    Added Folder model skeleton

---
+++ src/models/FolderModel.js
@@ -0,0 +1,16 @@
+/**
+ * FolderModel.js
+ */
+(function (spiderOakApp, window, undefined) {
+  "use strict";
+  var console = window.console || {};
+  console.log = console.log || function(){};
+  var Backbone    = window.Backbone,
+      _           = window._,
+      $           = window.$;
+
+  spiderOakApp.FolderModel = Backbone.Model.extend({
+    // ...
+  });
+
+})(window.spiderOakApp = window.spiderOakApp || {}, window);

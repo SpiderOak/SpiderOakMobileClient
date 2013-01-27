348e741a91136e7c4fa84412d826eb3c481e0289
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-01-27 23:43:56 +1100

    FolderView structure

--- src/views/FolderView.js
+++ src/views/FolderView.js
@@ -19,4 +19,37 @@
     // ...
   });
 
+  spiderOakApp.FolderListView = Backbone.View.extend({
+    tag: "ul",
+    className: "edgetoedge",
+    template: "", // _.template($('#folder-list-template').text()),
+    initialize: function() {
+      _.bindAll(this, "render", "addOne", "addAll");
+      this.collection.on( "add", this.addOne, this ); // might not be in use in read-only version
+      this.collection.on( "reset", this.addAll, this );
+      this.collection.on( "all", this.render, this );
+    },
+    render: function() {
+      // Add a "loading spinner" row at the top
+    },
+    addOne: function(model) {
+      // ...
+    },
+    addAll: function() {
+      // ...
+    }
+  });
+
+  spiderOakApp.FolderListItemView = Backbone.View.extend({
+    tag: "li",
+    className: "arrow",
+    template: "",
+    initialize: function() {
+      _.bindAll(this, "render");
+    },
+    render: function() {
+      // ...
+    }
+  });
+
 })(window.spiderOakApp = window.spiderOakApp || {}, window);

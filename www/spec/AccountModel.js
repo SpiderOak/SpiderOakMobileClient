a23030e36558b7e47f12ed78118bbafc53191aea
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-01-30 10:58:59 +1100

    Added TODO for cookie clearing

--- www/spec/AccountModel.js
+++ www/spec/AccountModel.js
@@ -206,6 +206,7 @@ describe('AccountModel', function() {
       });
       // @TODO: Clear keychain credentials test
       // @TODO: Clear any localStorage test
+      // @TODO: Clear out cookie if required
     });
   });
 

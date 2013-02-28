a9e6405d06939505251961735905a087346507f9
Ken Manheimer <ken.manheimer@gmail.com>
2013-02-28 15:45:51 -0500

    Relocate the cordova polyfills in a dedicated file, and load it immediately after the cordova.js load, so the polyfills are available as soon as possible, when necessary.
    Add a cordova polyfill for cordova.define, so we can blithely continue
    without the plugins, in so far as the lack of their functionality allows.
    
    (This enables us to continue without the FileViewerPlugin, for instance,
    without any accommodation within the plugin, itself.)

---
+++ www/cordova-polyfills.js
@@ -0,0 +1,34 @@
+if (! window.cordova) {
+
+  window.cordova = {bogus: "Cordova is absent, this stub is for polyfills."};
+
+  window.cordova['define'] = function(ident, thecode) {
+    var msg = "Cordova plugin '" + ident + "' ruthlessly skipped - no Cordova.";
+    window.console.log(msg);
+  };
+
+  if (! navigator.notification) {
+    navigator.notification = {};
+  }
+
+  if (! navigator.notification.alert) {
+    navigator.notification.alert =
+        function (message, alertCallback, title, buttonName) {
+          window.alert(message);
+          if (alertCallback) {
+            alertCallback();
+          }
+        };
+  }
+
+  if (! navigator.notification.confirm) {
+    navigator.notification.confirm =
+        function (message, confirmCallback, title, buttonLabels){
+          var isConfirmed = window.confirm(message);
+          if (confirmCallback)
+          {
+            confirmCallback((isConfirmed) ? 1 : 2);
+          }
+        };
+  }
+}

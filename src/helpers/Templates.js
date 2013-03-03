38cd18873bd4c6c21ffabb1ead4f6c55208b1ea4
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-03-03 11:49:19 +1100

    Clean up template loader

--- src/helpers/Templates.js
+++ src/helpers/Templates.js
@@ -7,16 +7,16 @@
       $           = window.$;
 
   window.tpl = {
-   
+
     // Hash of preloaded templates for the app
     templates: {},
-   
+
     // Recursively pre-load all the templates for the app.
     // @FIXME: Consider concating template files into one
     loadTemplates: function(templateLocation, names, callback) {
 
       var that = this;
-   
+
       var loadTemplate = function(index) {
         var name = names[index];
         console.log('Loading template: ' + name);
@@ -36,25 +36,16 @@
             console.warn(error);
           }
         });
-        // $.get(templateLocation + '/' + name + '.html', function(data) {
-        //   that.templates[name] = data;
-        //   index++;
-        //   if (index < names.length) {
-        //     loadTemplate(index);
-        //   } else {
-        //     callback();
-        //   }
-        // });
       };
-   
+
       loadTemplate(0);
     },
-   
+
     // Get template by name from hash of preloaded templates
     get: function(name) {
       return this.templates[name];
     }
-   
+
   };
 
 })(window);

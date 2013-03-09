ed302e02c208685d53bb424bd37e033f6c2b27d6
Ken Manheimer <ken.manheimer@gmail.com>
2013-03-09 17:53:34 -0500

    Guidance for model.composedUrl().

--- src/app.js
+++ src/app.js
@@ -111,6 +111,19 @@
     defaultEffect: (($.os.android) ? new window.BackStack.NoEffect() : null)
   });
 
+/*
+ * How a model in our framework determines its' composite URL.
+ *
+ * The URL is a concatenation of model and collection url elements.  Any
+ * prevailing elements that are functions are evaluated for their result.
+ *
+ * - A head part is taken in this order of precedence:
+ *   - the model's collection's urlBase attribute, or
+ *   - the model's collection's url attribute,
+ * - The url part is taken from the model's .get("url").
+ *
+ * @this{model}
+ */
 Backbone.Model.prototype.composedUrl = function() {
   var urlTail = this.get("url");
   var collection = this.collection;

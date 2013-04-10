10857d242af983b8fd66358ddd10cea641b3d230
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-04-10 15:44:17 +1000

    Implemented (including better API)

---
+++ src/models/StorageBarModel.js
@@ -0,0 +1,24 @@
+/**
+ * StorageBarModel.js
+ */
+(function (spiderOakApp, window, undefined) {
+  "use strict";
+  var console = window.console || {};
+  console.log = console.log || function(){};
+  var Backbone    = window.Backbone,
+      _           = window._,
+      $           = window.$;
+
+  spiderOakApp.StorageBarModel = Backbone.Model.extend({
+    parse: function(resp, xhr) {
+      var stats = resp.stats;
+      var size = resp.stats.size_for_robots;
+      var backupsize = parseInt(resp.stats.backupsize_for_robots, 10);
+      var percentUsed =
+        Math.ceil((backupsize / size) * 100);
+      stats.percentUsed = percentUsed;
+      return stats;
+    }
+  });
+
+})(window.spiderOakApp = window.spiderOakApp || {}, window);

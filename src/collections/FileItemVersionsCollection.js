706200b1c4dff7bc138e9a92e361ef842a8d1273
Ken Manheimer <ken.manheimer@gmail.com>
2013-05-06 17:38:22 -0400

    Include the collection's url, stripped of the filename, as the model's urlBase, when the model is added to the collection.  Then the vanilla model.composedUrl() will use that urlBase, rather than the collection's url.  This way, FileVersionModel doesn't need a tweaked duplicate .composedUrl().

--- src/collections/FileItemVersionsCollection.js
+++ src/collections/FileItemVersionsCollection.js
@@ -12,6 +12,15 @@
   var ppcb = spiderOakApp.PasswordProtectedCollectionBase;
   spiderOakApp.FileItemVersionsCollection = ppcb.extend({
     model: spiderOakApp.FileVersionModel,
+    initialize: function () {
+      ppcb.prototype.initialize.call(this);
+      // Supercede collections's url in model.composedUrl() by assigning the
+      // model with a 'usrBase', one that omits the file name:
+      this.on("add", function(model) {
+        model.set("urlBase",
+                  this.url.substring(0, this.url.lastIndexOf("/") + 1));
+      }.bind(this));
+    },
     parse: function(resp, xhr) {
       // window.console.log(resp);
       _.each(resp, function(file) {
--- src/models/FileVersionModel.js
+++ src/models/FileVersionModel.js
@@ -12,32 +12,6 @@
   spiderOakApp.FileVersionModel = spiderOakApp.FileModel.extend({
     defaults: {
       isFavorite: false
-    },
-    composedUrl: function(bare) {
-      var urlTail = this.get("url");
-      var collection = this.collection;
-      var urlHead = this.get("urlBase") || this.urlBase;
-      var urlHeadObject = urlHead && this;
-      if (! urlHead && collection) {
-        urlHead = (collection.get("urlBase") ||
-                   collection.urlBase ||
-                   collection.url ||
-                   "");
-        urlHeadObject = urlHead && collection;
-      }
-      if (typeof urlHead === "function") {
-        urlHead = urlHead.call(urlHeadObject);
-      }
-      if (typeof urlTail === "function") {
-        urlTail = urlTail.call(this);
-      }
-      // A version collection url contains the name of the file
-      // We don't want that in the individual model's url
-      urlHead = urlHead.substring(0, urlHead.lastIndexOf("/") + 1);
-      if (bare) {
-        urlHead = urlHead.split("?")[0];
-      }
-      return (urlHead || "") + (urlTail || "");
     }
   });
 

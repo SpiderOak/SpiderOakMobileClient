5504094f51f116a66ca95b287c04a8614946fdb3
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-12-05 19:41:24 +1100

    This is a few new images and a near total re-write of the versions hook
    I had to move the config hook from before_prepare to after_prepare because some things like the package id were being clobbered by the prepare stage.
    
    Then, because I did that, I have to move the versions hook as well, but since it was modifying config.xml prior to its use in the prepare stage, that method of modifying the version would no longer work.
    
    Since I already HATED the way I had done that anyway (constantly modifying a file under git control), I decided a refactor was in order. I changed the versions hook to use the values from the config and modify each platform's config file in-situ after having been copied by the config hook.

---
+++ .cordova/hooks/after_prepare/version.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+(function(require, console) {
+  var projectName = 'SpiderOak',
+    fs = require('fs'),
+    path = require('path'),
+    et = require('elementtree'),
+    plist = require('plist'),
+    shell = require('shelljs'),
+    projectRootDir = path.resolve(__dirname, '..', '..', '..'),
+    genericConfigFile = path.join(
+      projectRootDir,
+      'www',
+      'config.xml'
+    ),
+    iOSConfigFilePath = path.join(
+        projectRootDir,
+        'platforms',
+        'ios',
+        projectName
+    ),
+    androidConfigFilePath = path.join(
+        projectRootDir,
+        'platforms',
+        'android'
+    ),
+    version = shell.exec('git describe --tags', {silent:true}).output.trim();
+
+  if (fs.existsSync(iOSConfigFilePath)) {
+    var plistFile = path.join(iOSConfigFilePath, projectName + '-Info.plist');
+    var infoPlist = plist.parseFileSync(plistFile);
+    var config = new et.ElementTree(
+      et.XML(
+        fs.readFileSync(genericConfigFile, 'utf-8')
+      )
+    );
+    var package = config.getroot().attrib.id.split(".");
+    var version = config.getroot().attrib.version;
+    package.pop();
+    package.push(projectName);
+    infoPlist['CFBundleIdentifier'] = package.join(".");
+    infoPlist['CFBundleVersion'] = version;
+    infoPlist['CFBundleShortVersionString'] = version;
+    var info_contents = plist.build(infoPlist);
+    info_contents = info_contents.replace(/<string>[\s\r\n]*<\/string>/g,'<string></string>');
+    fs.writeFileSync(plistFile, info_contents, 'utf-8');
+  }
+
+  if (fs.existsSync(androidConfigFilePath)) {
+    var doc = new et.ElementTree(
+      et.XML(
+        fs.readFileSync(path.join(androidConfigFilePath, 'AndroidManifest.xml'), 'utf-8')
+      )
+    );
+    doc.getroot().attrib['android:versionName'] = version;
+    fs.writeFileSync(
+      path.join(androidConfigFilePath, 'AndroidManifest.xml'), doc.write({indent: 4}), 'utf-8'
+    );
+  }
+
+
+})(require, console);

94d8a1f3eff3bee099bd4c60ca7f5a1166eeeb57
Tommy-Carlos Williams <tommy@devgeeks.org>
2013-12-06 17:21:12 +1100

    Also no longer editing the file that the version in settings pulls from... fixed.

--- .cordova/hooks/after_prepare/version.js
+++ .cordova/hooks/after_prepare/version.js
@@ -18,11 +18,26 @@
         'ios',
         projectName
     ),
+    iOSConfigXMLFilePath = path.join(
+        projectRootDir,
+        'platforms',
+        'ios',
+        'www',
+        'config.xml'
+    ),
     androidConfigFilePath = path.join(
         projectRootDir,
         'platforms',
         'android'
     ),
+    androidConfigXMLFilePath = path.join(
+        projectRootDir,
+        'platforms',
+        'android',
+        'assets',
+        'www',
+        'config.xml'
+    )
     version = shell.exec('git describe --tags', {silent:true}).output.trim();
 
   if (fs.existsSync(iOSConfigFilePath)) {
@@ -42,6 +57,15 @@
     var info_contents = plist.build(infoPlist);
     info_contents = info_contents.replace(/<string>[\s\r\n]*<\/string>/g,'<string></string>');
     fs.writeFileSync(plistFile, info_contents, 'utf-8');
+    var configXML = new et.ElementTree(
+      et.XML(
+        fs.readFileSync(iOSConfigXMLFilePath, 'utf-8')
+      )
+    );
+    configXML.getroot().attrib.version = version;
+    fs.writeFileSync(
+      iOSConfigXMLFilePath, configXML.write({indent: 4}), 'utf-8'
+    );
   }
 
   if (fs.existsSync(androidConfigFilePath)) {
@@ -53,6 +77,15 @@
     doc.getroot().attrib['android:versionName'] = version;
     fs.writeFileSync(
       path.join(androidConfigFilePath, 'AndroidManifest.xml'), doc.write({indent: 4}), 'utf-8'
+    );
+    var configXML = new et.ElementTree(
+      et.XML(
+        fs.readFileSync(androidConfigXMLFilePath, 'utf-8')
+      )
+    );
+    configXML.getroot().attrib.version = version;
+    fs.writeFileSync(
+      androidConfigXMLFilePath, configXML.write({indent: 4}), 'utf-8'
     );
   }
 

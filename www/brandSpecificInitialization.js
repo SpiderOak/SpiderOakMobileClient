f7e2a154925baf81daa1315def5ed3bb648319f7
Ken Manheimer <ken.manheimer@gmail.com>
2014-05-09 19:26:00 -0400

    Incorporate custom brandSpecificInitialization.js.
    This is for brand-specific intialization actions, run immediately prior
    to app intialization in the onDeviceReady method.

---
+++ www/brandSpecificInitialization.js
@@ -0,0 +1,9 @@
+/* Initialization actions particular to this brand.
+ *
+ * Put a copy of this file with your desired initialization implementation in
+ * custom/brands/Whichever. It will be incoporated by the build
+ * customization process, replacing this file in the build.
+ */
+
+window.spiderOakApp.brandSpecificInitialization = function () {
+};
